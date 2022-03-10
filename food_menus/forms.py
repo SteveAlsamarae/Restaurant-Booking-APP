7b5c28aba4f0860e64acc9e0bea6560781484e0d
Steve Alsamarae <sarsaif@yahoo.com>
2022-03-11 01:24:10 +0600

    Add templates for Food Menus, fixes issue #8

--- food_menus/forms.py
+++ food_menus/forms.py
@@ -1,11 +1,21 @@
+from cProfile import label
 from django import forms
+from django.db import models
 
 from crispy_forms.helper import FormHelper
 
 from .models import MenuModel
+from .models import FOOD_ITEM_VALIDATOR
 
 
 class MenuForm(forms.ModelForm):
+    name = forms.CharField(max_length=120, label="Item Name", help_text="i.e. Chicken Burger")
+    food_items = forms.CharField(
+        max_length=300,
+        validators=[FOOD_ITEM_VALIDATOR],
+        help_text="i.e. 'pizza, pasta, salad'",
+    )
+
     class Meta:
         model = MenuModel
         fields = ["name", "menu_image", "price", "food_items"]
